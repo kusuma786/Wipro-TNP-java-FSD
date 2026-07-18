15c2840d78370c1e7c9b252d8ecbd81fb1ec2fcc
Kusuma <kusumaaswiniraavi@gmail.com>
2026-07-18 13:02:00 +0530

    Completed Language Basics Exercise 3

---
+++ src/languagebasics/Exercise3.java
@@ -0,0 +1,19 @@
+package languagebasics;
+
+public class Exercise3 {
+
+    public static void main(String[] args) {
+
+        if (args.length != 2) {
+            System.out.println("Please provide two integer arguments.");
+            return;
+        }
+
+        int num1 = Integer.parseInt(args[0]);
+        int num2 = Integer.parseInt(args[1]);
+
+        int sum = num1 + num2;
+
+        System.out.println("The sum of " + num1 + " and " + num2 + " is " + sum);
+    }
+}
