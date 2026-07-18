299e01a8f597a9deeaa1dfc703e75b6fdeb3bb63
Kusuma <kusumaaswiniraavi@gmail.com>
2026-07-18 12:59:46 +0530

    Completed Language Basics Exercise 2

---
+++ src/languagebasics/Exercise2.java
@@ -0,0 +1,14 @@
+package languagebasics;
+
+public class Exercise2 {
+
+    public static void main(String[] args) {
+
+        if (args.length != 1) {
+            System.out.println("Please provide one command line argument.");
+            return;
+        }
+
+        System.out.println("Welcome " + args[0]);
+    }
+}
